cd84ded705588c120d2f870090730830070d55a6
OuMniaCh <s3427382@vuw.leidenuniv.nl>
2025-03-01 12:43:28 +0100

    Step10: app.post route code

--- index.js
+++ index.js
@@ -21,15 +21,36 @@ const headers = {
 // * Code for Route 1 goes here
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
-
 app.get("/update-cobj", (req, res) => {
     res.render("updates", {
       title: "Update Custom Object Form | Integrating With HubSpot I Practicum",
     });
   });
+  
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
-
+app.post("/update-cobj", async (req, res) => {
+    const url = `https://api.hubapi.com/crm/v3/objects/${OBJECT_TYPE_ID}`;
+  
+    const body = {
+      properties: {
+        pet_name: req.body["pet_name"],
+        pet_food: req.body["pet_food"],
+        pet_type: req.body["pet_type"]
+      },
+    };
+  
+    try {
+      await axios.post(url, body, { headers });
+      res.redirect("/");
+    } catch (err) {
+      console.error(err);
+      res.render("updates", {
+        title: "Update Custom Object Form | Integrating With HubSpot I Practicum",
+        error: "There was an issue processing your request. Please try again.",
+      });
+    }
+  });
 
 // * Localhost
 app.listen(3000, () => console.log('Listening on http://localhost:3000'));
